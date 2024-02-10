221b3d45b258555df0151868c7b806b1b5b9949b
chaitanyaram1204 <chaitanyaamajala@gmail.com>
2024-02-10 20:35:44 +0530

    MS Dhoni

--- content.py
+++ content.py
@@ -1,2 +1,22 @@
 from config import key
-print(key)
+import requests #web 
+
+def getcontent(chat):
+    data = {
+        "contents" : [
+            {
+                "role" : "user",
+                "parts" : [
+                    {
+                        "text" : "You are an AI bot,your name is My jarvis" + " " + chat
+                    }
+                ]
+            }
+        ]
+    }
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="+key
+    response = requests.post(url,json = data)
+    t1 = response.json()
+    print(t1)
+msg = "How is ms dhoni"
+getcontent(msg)
