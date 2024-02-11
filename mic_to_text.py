0b76d64f81b3c17241d7195b624879547b46c3be
chaitanyaram1204 <chaitanyaamajala@gmail.com>
2024-02-11 10:46:18 +0530

    Take voice input

--- mic_to_text.py
+++ mic_to_text.py
@@ -1,5 +1,17 @@
 import speech_recognition as sr
-index = 0
-for name in sr.Microphone.list_microphone_names():
+#index = 0
+'''for name in sr.Microphone.list_microphone_names():
     print(index,' ' ,name)
-    index+=1
+    index+=1'''
+recognizer = sr.Recognizer()
+def mic_to_text_class():
+    with sr.Microphone(device_index = 0) as source:
+        print("Say something:")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+        print("Recognizing....") 
+    try:
+        print("You said :",recognizer.recognize_google(audio))
+    except:
+        pass
+    return recognizer.recognize_google(audio)
