8b9c1c43b16f35bc1e65aca35ced3d93233c9e5e
chaitanyaram1204 <chaitanyaamajala@gmail.com>
2024-02-11 22:02:00 +0530

    Complete Front End Code

---
+++ agent.py
@@ -0,0 +1,70 @@
+#this find executes the respective function based on the query
+from config import key
+import requests #web
+import task1
+
+def parse_function_response(message):
+    function_call = message[0].get("functionCall")
+    function_name = function_call.get("name")
+    """ parameters = function_call.get("parameters")
+    if function_name == "temp_city":
+        city = parameters.get("city")
+        return task1.temp_city(city)
+    elif function_name == "temp_room":
+        room = parameters.get("room")
+        return task1.temp_room(room)
+    else:
+        return "Function not found" """
+    print("Gemini : call function ",function_name)
+
+    try :
+        arguments = function_call.get("args")
+        print("Gemini : arguments are ",arguments)
+        if arguments :
+            d = getattr(task1,function_name)
+            print("function is : ",d)
+            function_response = d(**arguments)
+    except Exception as e:
+        print(e)
+        function_response = "Error : Function not found"
+    return function_response
+def run_conversion(user_input):
+    data = {
+        "contents" : [
+            {
+                "role" : "user",
+                "parts" : [
+                    {
+                        "text" : "You are an AI bot that can do everything using function call.When you are asked to do something, use the function call available and then respond with message" + "\n" + user_input
+                    }
+                ]
+            }
+        ],
+        "tools":
+        [
+            {
+                "functionDeclarations": task1.definitions
+            }
+        ]
+    }
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="+key
+    response = requests.post(url,json = data)
+
+    if response.status_code != 200:
+        print(response.text)    
+    
+    reply_api = response.json()
+    if "content" not in reply_api.get("candidates")[0]:
+        print("Error : No content in response")
+        #return "No content found"
+    message = reply_api.get("candidates")[0].get("content").get("parts")
+
+
+    if 'functionCall' in message[0]:
+        resp1 = parse_function_response(message)
+        return resp1
+    """   reply = reply_api.get("candidates")[0].get("content").get("parts")[0].get("text") """
+    print(reply_api)
+
+if __name__ == "__main__":
+    print(run_conversion("find the ip addres of google.com"))
--- task1.py
+++ task1.py
@@ -1,12 +1,23 @@
 import requests
+import socket
 
+def get_ip(host):
+    try:
+        ip = socket.getaddrinfo("google.com",None)
+        return ip
+    except:
+        return "Error : Host not found"
+
+def temp_room(room):
+    result = "Temperatiure in " + room + " is 25°C"
+    return result
 def temp_city(city):
     url = "https://yahoo-weather5.p.rapidapi.com/weather"
 
     querystring = {"location":city,"format":"json","u":"f"}
 
     headers = {
-        "X-RapidAPI-Key": "zx",
+        "X-RapidAPI-Key": "6df66e6f06msh5f697bd21eaf46bp1db5edjsnf8c8eefe107d",
         "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
     }
 
@@ -18,6 +29,45 @@ def temp_city(city):
     temp = d1.get("condition").get("temperature")
     temp = round((temp-32)*5/9,2)
     return f"Temperature in {city} is {temp}°C and humidity is {hum}%"
-
+definitions =[
+        {
+            "name" : "temp_city",
+            "description" : "This function takes a city name as input and returns the temperature and humidity of the city in celsius and percentage respectively.",
+            "parameters" :
+                {"type": "object",
+                "properties":
+                            {"city": 
+                                {"type": "string",
+                                "description": "Name of the city for which temperature and humidity is to be found."
+                                }
+                            }
+                },
+        },
+        {
+            "name" : "temp_room",
+            "description" : "Find the temperature of my room or my home.",
+            "parameters" :
+                {"type": "object",
+                "properties":
+                            {"room": 
+                                {"type": "string",
+                                "description": "room or home"
+                                }
+                            }
+                },
+        },{
+            "name" : "get_ip",
+            "description" : "Gfind ip address of given url or domain name.",
+            "parameters" :
+                {"type": "object",
+                "properties":
+                            {"host": 
+                                {"type": "string",
+                                "description": "get url or domain name."
+                                }
+                            }
+                },
+        }
+]
 if __name__ == "__main__":
-    print(temp_city("delhi"))
+    print(temp_city("vijayawada"))
